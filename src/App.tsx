3938ad69c79f0bbb8d4443a6fde0a40178741d5e
zyz <zyzisgood@gmail.com>
2020-04-25 17:19:59 -0500

    first useState update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Hello from './components/Hello';
+import LikeButton from './components/LikeButton';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Hello message="hello world 2"></Hello>
+        <LikeButton />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,19 @@
+import React,{useState} from 'react';
+
+const LikeButton:React.FC = ()=> {
+    const [like,setLike] = useState(0);
+    const [on,setOn] = useState(true);
+
+    return (
+        <>
+            <button onClick = {()=> {setLike(like + 1)}}>
+                {like}
+            </button>
+            <button onClick = {()=> {setOn(!on)}}>
+                {on ? 'ON':'OFF'}
+            </button>
+        </>
+    )
+}
+
+export default LikeButton;
